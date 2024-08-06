1b1bcd3d4663f1b74256ec881dd3832895cc7fd6
Garrett Albers <garrett.albers@pura>
2024-08-06 09:20:27 -0700

    Eliminate preceding white-space

--- sdr2mqtt/entry.sh
+++ sdr2mqtt/entry.sh
@@ -30,7 +30,7 @@ export LD_LIBRARY_PATH=/usr/local/lib64
 
 bashio::log.info "========================================="
 # RTL_SDR_GET_DEVICES="$(rtl_sdr -d 9999)"
-rtl_sdr -d 9999 |& grep "SN: ${RTL_SDR_SERIAL_NUM}" |& grep -o '^[^:]*'
+rtl_sdr -d 9999 |& grep "SN: ${RTL_SDR_SERIAL_NUM}" |& grep -o '^[^:]*' | sed 's/^[ \t]*//;s/[ \t]*$//')
 # bashio::log.info "RTL-SDR's found =" $RTL_SDR_GET_DEVICES
 
 bashio::log.info "=============GREP RESULT================="
