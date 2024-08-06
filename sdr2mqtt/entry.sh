125d9d72448540e0f0e046312094e5870e9ccfce
Garrett Albers <garrett.albers@pura>
2024-08-06 10:28:01 -0700

    Attempt doing lookup in shell script again

--- sdr2mqtt/entry.sh
+++ sdr2mqtt/entry.sh
@@ -32,9 +32,10 @@ bashio::log.info "========================================="
 
 # rtl_sdr -d 9999 |& grep "SN: ${RTL_SDR_SERIAL_NUM}" |& grep -o '^[^:]*' | sed 's/^[ \t]*//;s/[ \t]*$//'
 # RTL_INDEX=`rtl_sdr -d 9999 |& grep "SN: ${RTL_SDR_SERIAL_NUM}" |& grep -o '^[^:]*' | sed 's/^[ \t]*//;s/[ \t]*$//'`
+RTL_INDEX=$(rtl_sdr -d 9999 |& grep "SN: $RTL_SDR_SERIAL_NUM" |& grep -o '^[^:]*' | sed 's/^[ \t]*//;s/[ \t]*$//')
 
 bashio::log.info "=========GET INDEX========="
-bashio::log.info "RTL_INDEX =" $1
+bashio::log.info "RTL_INDEX =" $RTL_INDEX
 # bashio::log.info "RTL_INDEX =" rtl_sdr -d 9999 |& grep "SN: 433" |& grep -o '^[^:]*' | sed 's/^[ \t]*//;s/[ \t]*$//'
 
 # bashio::log.info "RTL-SDR's found =" $RTL_SDR_GET_DEVICES
